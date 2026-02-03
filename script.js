d0aa59e4e3b93264efcc013153910416fd440ee9
firesareba <101755187+firesareba@users.noreply.github.com>
2026-02-03 13:04:10 -0800

    outlines of big boxes in 3 corners

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 //#region access html
-const url = document.getElementById("url");
+const url_input = document.getElementById("url");
 
 const canvas = document.getElementById("code-canvas")
 const drawable_canvas = canvas.getContext("2d");
@@ -12,9 +12,65 @@ code_grid = []
 //#endregion
 
 //#region listeners
-url.addEventListener(
+url_input.addEventListener(
     "change", function(event) {
-        console.log('new url')
+        generateCode()
     }
 );
 //#endregion
+
+function reset(){
+    code_grid = []
+    for (let i=0; i<25; i++){
+        code_grid.push([]);
+        for (let j=0; j<25; j++){
+            code_grid[i].push(0);
+        }
+    }
+
+    //top-left
+    for (let i=0; i<7; i++){
+        code_grid[0][i] = 1;
+    }
+    for (let i=1; i<6; i++){
+        code_grid[i][0] = 1;
+        code_grid[i][6] = 1;
+    }
+    for (let i=0; i<7; i++){
+        code_grid[6][i] = 1;
+    }
+
+    //top right
+    for (let i=0; i<7; i++){
+        code_grid[0][24-i] = 1;
+    }
+    for (let i=1; i<6; i++){
+        code_grid[i][24] = 1;
+        code_grid[i][18] = 1;
+    }
+    for (let i=0; i<7; i++){
+        code_grid[6][24-i] = 1;
+    }
+
+    //bottom left
+    for (let i=0; i<7; i++){
+        code_grid[24][i] = 1;
+    }
+    for (let i=1; i<6; i++){
+        code_grid[24-i][0] = 1;
+        code_grid[24-i][6] = 1;
+    }
+    for (let i=0; i<7; i++){
+        code_grid[18][i] = 1;
+    }
+}
+
+function generateCode(){
+    reset();
+
+    console.log(code_grid);
+    // url = url_input.value;
+    // for (let i = 0; i < url.length; i++){
+    //     console.log(url.charCodeAt(i))
+    // }
+}
