a9e51a16ac4d4969a22b61de6c37e4a3f0428bb9
firesareba <101755187+firesareba@users.noreply.github.com>
2026-02-03 17:35:32 -0800

    2 is for base, 1 is for data 1

--- script.js
+++ script.js
@@ -31,16 +31,16 @@ function reset(){
     //#region top-left
     //outline
     for (let i=0; i<7; i++){
-        code_grid[0][i] = 1;//top row
-        code_grid[i][0] = 1;//left column
-        code_grid[i][6] = 1;//right column
-        code_grid[6][i] = 1;//bottom column
+        code_grid[0][i] = 2;//top row
+        code_grid[i][0] = 2;//left column
+        code_grid[i][6] = 2;//right column
+        code_grid[6][i] = 2;//bottom column
     }
 
     //middle
     for (let i=2; i<=4; i++){
         for (let j=2; j<=4; j++){
-            code_grid[i][j] = 1;
+            code_grid[i][j] = 2;
         }
     }
     //#endregion
@@ -48,16 +48,16 @@ function reset(){
     //#region top right
     //outline
     for (let i=0; i<7; i++){
-        code_grid[0][24-i] = 1;//top row
-        code_grid[i][24] = 1;//right column
-        code_grid[i][18] = 1;//left column
-        code_grid[6][24-i] = 1;//bottom row
+        code_grid[0][24-i] = 2;//top row
+        code_grid[i][24] = 2;//right column
+        code_grid[i][18] = 2;//left column
+        code_grid[6][24-i] = 2;//bottom row
     }
 
     //middle
     for (let i=2; i<=4; i++){
         for (let j=22; j>=20; j--){
-            code_grid[i][j] = 1;
+            code_grid[i][j] = 2;
         }
     }
     //#endregion
@@ -65,39 +65,39 @@ function reset(){
     //#region bottom left
     //outline
     for (let i=0; i<7; i++){
-        code_grid[24][i] = 1;//bottom row
-        code_grid[24-i][0] = 1;//left column
-        code_grid[24-i][6] = 1;//right column
-        code_grid[18][i] = 1;//top row
+        code_grid[24][i] = 2;//bottom row
+        code_grid[24-i][0] = 2;//left column
+        code_grid[24-i][6] = 2;//right column
+        code_grid[18][i] = 2;//top row
     }
 
     //middle
     for (let i=22; i>=20; i--){
         for (let j=2; j<=4; j++){
-            code_grid[i][j] = 1;
+            code_grid[i][j] = 2;
         }
     }
     //#endregion
 
     //#region smaller extra
     for (let i=0; i<=4; i++){
-        code_grid[16][16+i] = 1;//top row
-        code_grid[16+i][16] = 1;//left column
-        code_grid[16+i][20] = 1;//right column
-        code_grid[20][16+i] = 1;//bottom row
+        code_grid[16][16+i] = 2;//top row
+        code_grid[16+i][16] = 2;//left column
+        code_grid[16+i][20] = 2;//right column
+        code_grid[20][16+i] = 2;//bottom row
     }
-    code_grid[18][18] = 1;//center
+    code_grid[18][18] = 2;//center
     //#endregion
 
     //#region timing strips
     for (let i=8; i<=16; i+=2){
-        code_grid[6][i] = 1;
-        code_grid[i][6] = 1;
+        code_grid[6][i] = 2;
+        code_grid[i][6] = 2;
     }
     //#endregion
 
-    code_grid[17][8] = 1;//random one in all qr codes
-    code_grid[24][23] = 1; //mode indicator 0100 for binary mode(goes right ot left, bottom to top.)
+    code_grid[17][8] = 2;//random one in all qr codes
+    code_grid[24][23] = 2; //mode indicator 0100 for binary mode(goes right ot left, bottom to top.)
 
     drawable_canvas.fillStyle = "white";
     drawable_canvas.fillRect(0, 0, 27*cell_size, 27*cell_size);
@@ -107,10 +107,17 @@ function reset(){
 function generateCode(){
     reset();
 
-    displayCode();
     url = url_input.value;
+    console.log((url.length).toString(2));
+
     for (let i = 0; i < url.length; i++){
         console.log(url.charCodeAt(i))
+    }
+    displayCode();
+}
+
+function writeByte(byte, start){
+    for (let i = 0; i<8; i++){
     }
 }
 
